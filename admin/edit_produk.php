b0932db7be94122abf6c320f33f091f3b39b21f4
Fiskaperdana Nuraji <fiska_xp2@yahoo.com>
2013-05-08 06:51:21 +0700

    Update edit_produk.php

--- admin/edit_produk.php
+++ admin/edit_produk.php
@@ -25,7 +25,7 @@ $sql=mysql_query("SELECT * FROM produk WHERE id_produk='$id_produk'");
 $r=mysql_fetch_array($sql)
 ?>
 <div id="contain">
-<form action="aksiedit_produk.php?id_produk=<?php echo '$r[id_produk]';?>" method="post" enctype="multipart/form-data">
+<form action="aksiedit_produk.php?id_produk=<?php echo $r['id_produk'];?>" method="post" enctype="multipart/form-data">
 <input type="hidden" name="id_produk" value="<?php echo $r['id_produk']; ?>">
 <table border="1px" width="650" align="center">
 <tr>
@@ -33,17 +33,17 @@ $r=mysql_fetch_array($sql)
 	<td>:</td>
 	<td><select name="nama_ktgproduk">
 	<?php 
-$sql2=mysql_query("SELECT * FROM ktgproduk WHERE id_ktgproduk='$id_ktgproduk'");
-$data2=mysql_fetch_array($sql2);
+//$sql2=mysql_query('SELECT * FROM ktgproduk WHERE id_ktgproduk='.$id_ktgproduk);
+//$data2=mysql_fetch_array($sql2);
 
 	$sql3=mysql_query("SELECT * FROM  ktgproduk");
 	while ($w=mysql_fetch_array($sql3)){
 		if($data['nama_ktgproduk']==$w['id_ktgproduk']){
-			echo"<option value='$w[id_ktgproduk]'SELECTED>$w[nama_ktgproduk]</option>";
+			echo'<option value="'.$w['id_ktgproduk'].'" SELECTED>'.$w[nama_ktgproduk].'</option>';
 		
 		}else
 		{
-			echo "<option value='$w[id_ktgproduk]'>$w[nama_ktgproduk]</option>";
+			echo '<option value="'.$w[id_ktgproduk].'">'.$w['nama_ktgproduk'].'</option>';
 		}
 	}
 	?></select></td>
@@ -70,7 +70,8 @@ $data2=mysql_fetch_array($sql2);
 	<tr>
 	<td>Gambar Lama</td>
 	<td>:</td>
-	<td><img src="images/<?php echo $r['gbr_produk'];?>" width="100"></td>
+	<td><img src="images/<?php echo $r['gbr_produk'];?>" width="100""></td>
+	<input type="hidden" value="<?php echo $r['gbr_produk'];?>" name="noupdate" >
 	</tr>
     
 	<tr>
